a291b204d4fb1285e7de311d34c9b507d9bd5b6f
SGoyal7 <goyalshubham7@gmail.com>
2021-03-23 23:04:44 +0530

    intial commit

---
+++ Tut Practise/Vuejs with CDN/app.js
@@ -0,0 +1,4 @@
+const app = Vue.createApp({
+	template: "<h2><h1>Hello vue3</h1>Hello with Vue CDN</h2>",
+});
+app.mount("#app");
---
+++ blog/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h1>Hello from Home component</h1>
+		<h1>{{ data }}</h1>
+		<h1>{{ msg }}</h1>
+		<button v-on:click="hello('data')">Call Hello Function</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Home",
+	props: {
+		data: String,
+		msg: String,
+	},
+};
+</script>
